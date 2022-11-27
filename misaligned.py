c53f8268adbed9de2611df6b313d2bb441c66720
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2022-11-27 22:50:16 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,23 +1,14 @@
-
+major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+colorCode = {}
 def print_color_map():
-    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-    for i, major in enumerate(major_colors):
-        for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
-    return len(major_colors) * len(minor_colors)
-def getColorCode(major_color,minor_color):
-    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-    colorCode = {}
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
             print(f'{i * 5 + j} | {major} | {minor}')
             colorCode[str({major} | {minor})] = i * 5 + j
-    return colorCode[str({major_color} | {minor_color})]
-
+    return len(major_colors) * len(minor_colors)
 result = print_color_map()
 assert(result == 25)
-assert(getColorCode("Red","Orange") == 7)
-assert(getColorCode("Violet","Green") == 21)
+assert(colorCode[str({"Red"} | {"Orange"})] == 7)
+assert(colorCode[str({"Violet"} | {"Green"})] == 23)
 print("All is well (maybe!)\n")
