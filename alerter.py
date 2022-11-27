f36138ce2ab4d6d2b6bd86dd6414262de2ec776b
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2022-11-27 22:42:41 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -26,4 +26,5 @@ def alert_in_celcius(farenheit):
 alert_in_celcius(400.5)
 alert_in_celcius(303.6)
 print(f'{alert_failure_count} alerts failed.')
+assert(alert_failure_count==0)
 print('All is well (maybe!)')
